b13a09f1b1a30f09e7ccd7d05858b340734ae66a
Micadurp <neoswed@hotmail.com>
2017-02-02 08:44:01 +0100

    Added warning about memory leaks and SFML

--- Game/Source/main.cpp
+++ Game/Source/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Piece of shit SFML will show some memory leaks that don't exist
 	System system;
 
 	if (system.Initialize())
