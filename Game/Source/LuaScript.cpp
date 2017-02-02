cad7e2f9f485ee1bfb78464b5fac821f85fafe26
Micadurp <neoswed@hotmail.com>
2017-02-02 08:40:23 +0100

    Attempt att adding lua to project

---
+++ Game/Source/LuaScript.cpp
@@ -0,0 +1,20 @@
+#include <LuaScript.h>
+
+LuaScript::LuaScript(const std::string& filename) 
+{
+	L = luaL_newstate();
+	if (luaL_loadfile(L, filename.c_str()) || lua_pcall(L, 0, 0, 0)) {
+		std::cout << "Error: script not loaded (" << filename << ")" << std::endl;
+		L = 0;
+	}
+}
+
+LuaScript::~LuaScript() 
+{
+	if (L) lua_close(L);
+}
+
+void LuaScript::PrintError(const std::string& variableName, const std::string& reason) 
+{
+	std::cout << "Error: can't get [" << variableName << "]. " << reason << std::endl;
+}
